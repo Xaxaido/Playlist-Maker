526937fb22089ee143f47f5659dacfa254cb4bb8
Xaxaido <xaxaido@gmail.com>
2024-09-26 13:59:24 +0300

    Домашнее задание, спринт 18

---
+++ app/src/main/java/com/practicum/playlistmaker/medialibrary/view_model/PlaylistsViewModel.kt
@@ -0,0 +1,9 @@
+package com.practicum.playlistmaker.medialibrary.view_model
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class PlaylistsViewModel @Inject constructor(): ViewModel() {
+}
